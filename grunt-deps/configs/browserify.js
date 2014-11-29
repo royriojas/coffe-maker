c5258995dd20a83d9e8bca4124ceeea6f04c4ec8
Roy Riojas <roy@kno.com>
2014-11-29 01:13:51 -0800

    ENH: Use custom transforms for browserify

--- grunt-deps/configs/browserify.js
+++ grunt-deps/configs/browserify.js
@@ -1,13 +1,32 @@
 module.exports = function ( grunt, pkg, opts ) {
   'use strict';
 
+  var deps = {
+    'window': 'global.window',
+    'jQuery': 'global.jQuery',
+    'screen': 'global.screen',
+    'Worker': 'global.Worker',
+    'Promise': 'global.Promise',
+    'self': 'global.self'
+  };
+
   return {
     options: {
       preBundleCB: function ( b ) {
-        b.transform( require( 'build-workflow/utils/require-arr' ));
-        b.transform( {
-          global: true
-        }, 'browserify-shim' );
+        var transforms = [
+          require( 'build-workflow/utils/shimify' ).configure( deps ),
+          require( 'build-workflow/utils/strictify' ),
+          require( 'build-workflow/utils/consoleify' ),
+          require( 'build-workflow/utils/dotify' ),
+          require( 'build-workflow/utils/require-arr' )
+        ];
+
+        transforms.forEach(function ( dep ) {
+          b.transform( {
+            global: true
+          }, dep );
+        } );
+
         return b;
       }
     },
